41410d1f48f5a4437588ac1cf8258055a06be348
Jam516 <kufuorkofi@gmail.com>
2025-02-19 15:45:00 +0000

    commit

--- app/actions/getHomeData.tsx
+++ app/actions/getHomeData.tsx
@@ -1,6 +1,30 @@
+interface LeaderboardEntry {
+    PROJECT: string
+    LOGO: string
+    PAYMASTER_VOLUME_7D: number
+    ACTIVE_ACCOUNTS_7D: number
+    GASLESS_TXNS_7D: number
+    RN_7D: number
+    PAYMASTER_VOLUME_30D: number
+    ACTIVE_ACCOUNTS_30D: number
+    GASLESS_TXNS_30D: number
+    RN_30D: number
+    PAYMASTER_VOLUME_90D: number
+    ACTIVE_ACCOUNTS_90D: number
+    GASLESS_TXNS_90D: number
+    RN_90D: number
+    WEBSITE: string | null
+    CATEGORY: string
+}
+
+interface PaymasterStats {
+    GASLESS_TXNS: number;
+    PAYMASTER_VOLUME: number;
+}
+
 interface HomeData {
-    leaderboard: any[];
-    total_paymaster_stats: any[];
+    leaderboard: LeaderboardEntry[];
+    total_paymaster_stats: PaymasterStats[];
 }
 
 interface HomeDataParams {
