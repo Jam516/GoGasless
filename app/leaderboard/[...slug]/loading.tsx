40f635756b5abf7bc5e8132e56a1ac77fc29a8b0
Jam516 <kufuorkofi@gmail.com>
2025-03-07 12:16:44 +0000

    commit

--- app/leaderboard/[...slug]/loading.tsx
+++ app/leaderboard/[...slug]/loading.tsx
@@ -1,4 +1,4 @@
-import { MobileBlocker } from "@/components/mobile-blocker";
+// import { MobileBlocker } from "@/components/mobile-blocker";
 import { Skeleton } from "@/components/ui/skeleton"
 
 
@@ -7,7 +7,7 @@ export default function Loading() {
     return (
         <div className="flex flex-col mt-10 mb-10 font-[family-name:var(--font-inter-sans)]">
             <main className="flex flex-col items-center space-y-12">
-                <MobileBlocker />
+                {/* <MobileBlocker /> */}
                 <div className="flex flex-col gap-4 items-center md:w-1/2">
                     <h1 className="text-xl md:text-4xl font-bold leading-none tracking-tight text-center">Discover Gasless Apps</h1>
                     <p className="text-sm md:text-base font-semibold text-muted-foreground text-center">Gas fees are the biggest barrier to onboarding new crypto users. Use {" "}
